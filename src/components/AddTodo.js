2470dd2064af5a3065203d7ecc2cb9a104980af5
ilamidi <dgoldenone@gmail.com>
2020-05-17 10:17:49 +0100

    minor update from second lappy

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,13 +1,36 @@
 import React, {Component} from 'react';
 
 export class AddTodo extends Component {
+
+    state = {
+        title: ''
+    }
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.addTodo(this.state.title);
+        this.setState({title: ''});
+    }
+
+    onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
     render() {
         return (
-            <form style={{ display: 'flex' }}>
-                <input type='text' name="title" placeholder="Add Todo..." style={{ flex: '10', padding: '5px' }} />
-                <input type="submit" className="btn" value="Submit" style={
-                    {flex: '1'}
-                }/>
+            <form onSubmit = {this.onSubmit} style={{ display: 'flex' }}>
+                <input 
+                type='text' 
+                name="title" 
+                placeholder="Add Todo..." 
+                value={this.state.title} 
+                style={{ flex: '10', padding: '5px' }} 
+                onChange = {this.onChange}
+                />
+
+                <input 
+                type="submit" 
+                className="btn" 
+                value="Submit" 
+                style={{flex: '1'}}
+                />
             </form>
         )
     }
